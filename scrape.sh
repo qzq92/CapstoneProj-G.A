2ff594ed45145c77c4591f39590c986732eb20e5
Quek Zhi Qiang <quekzhiqiang92@hotmail.com>
2022-12-11 21:40:51 +0800

    Tested script, ok to run

--- scrape.sh
+++ scrape.sh
@@ -17,18 +17,18 @@ AGENT_ID=Capstone_QZQ
 # Logfile path
 LOG_FILE=errors.log
 
-# Endtime to stop scraping (dd/mm/yyyy hh:mm format)
-END_TIME=11/12/2022 17:00
+# Endtime to stop scraping (dd/mm/yyyy-hh:mm format)
+END_TIME=11/12/2022-21:37
 
 # Extracting traffic camera IDs and coordinates mappings. Comment out either of the following code blocks if you do not intend to run the specific operations.
-echo "Extracting traffic camera IDs and coordinates mapping."
-echo " "
-python scrape_footage_camera_loc.py get_cctv_coordinates --lta_api $LTA_API_URL --camera_coord_metadata $CAMERA_COORD_FILE --agent_id $AGENT_ID --log_file $LOG_FILE 
+#echo "Extracting traffic camera IDs and coordinates mapping."
+#echo " "
+#python scrape_footage_camera_loc.py get_cctv_coordinates --lta_api $LTA_API_URL --camera_coord_metadata $CAMERA_COORD_FILE --agent_id $AGENT_ID --log_file $LOG_FILE 
 
 # Scraping of traffic footages periodically till specified end time. Comment out either of the following code blocks if you do not intend to use any of the model.
-# echo "Scraping traffic footages via LTA API URL until $END_TIME"
-# echo " "
-# python scrape_footage_camera_loc.py scrape --lta_api $LTA_API_URL --metadata_file $METADATA_FILE --agent_id $AGENT_ID --log_file $LOG_FILE --end_time $END_TIME
+ echo "Scraping traffic footages via LTA API URL..."
+ echo " "
+ python scrape_footage_camera_loc.py extract_footage --lta_api $LTA_API_URL --metadata_file $METADATA_FILE --agent_id $AGENT_ID --log_file $LOG_FILE --end_time $END_TIME
 
 echo "Exiting program."
 exit 0
